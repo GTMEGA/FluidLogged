3662601c29d8ea5aff6edcbce487fa3fe17c032c
FalsePattern <me@falsepattern.com>
2025-02-02 03:15:38 +0100

    license tidy

--- src/main/java/mega/fluidlogged/internal/core/CoreLoadingPlugin.java
+++ src/main/java/mega/fluidlogged/internal/core/CoreLoadingPlugin.java
@@ -1,27 +1,23 @@
 /*
- * ChunkAPI
+ * This file is part of FluidLogged.
  *
- * Copyright (C) 2023-2024 FalsePattern
+ * Copyright (C) 2025 The MEGA Team, FalsePattern
  * All Rights Reserved
  *
- * The above copyright notice and this permission notice
+ * The above copyright notice, this permission notice and the word "MEGA"
  * shall be included in all copies or substantial portions of the Software.
  *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
+ * FluidLogged is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, only version 3 of the License.
  *
- * This program is distributed in the hope that it will be useful,
+ * FluidLogged is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program. If not, see <https://www.gnu.org/licenses/>.
- *
- * This program comes with additional permissions according to Section 7 of the
- * GNU Affero General Public License. See the full LICENSE file for details.
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with FluidLogged.  If not, see <https://www.gnu.org/licenses/>.
  */
 
 package mega.fluidlogged.internal.core;
